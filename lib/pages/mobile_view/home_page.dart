7117f8e7cc66e8058ba50460975a67d2fb4666cf
Shafi Munshi <shafimunshi111@gmail.com>
2022-03-31 14:04:04 -0700

    third

--- lib/pages/mobile_view/home_page.dart
+++ lib/pages/mobile_view/home_page.dart
@@ -17,48 +17,13 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int indexx = 0;
-
-  List<Widget> question = [
-    InkWell(
-      onTap: () {},
-      child: Container(
-        alignment: Alignment.center,
-        height: 100,
-        width: double.infinity,
-        decoration: BoxDecoration(
-            color: Colors.white, borderRadius: BorderRadius.circular(10)),
-        child: Text(
-          'Loading',
-          style: GoogleFonts.ptSerif(
-              fontWeight: FontWeight.bold,
-              color: Appcolor.cnColor,
-              fontSize: 33.sp),
-        ),
-      ),
-    ),
-    InkWell(
-      onTap: () {},
-      child: Container(
-        alignment: Alignment.center,
-        height: 130,
-        width: double.infinity,
-        decoration: BoxDecoration(
-            border: Border.all(
-              color: Appcolor.cnColor,
-              width: 2,
-            ),
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10)),
-        child: Text(
-          'Why do you remember for school?',
-          textAlign: TextAlign.center,
-          style: GoogleFonts.ptSerif(
-              fontWeight: FontWeight.bold,
-              color: Appcolor.cnColor,
-              fontSize: 33),
-        ),
-      ),
-    ),
+  List<String> que = [
+    'Loading...',
+    'Who do you remebe from school?',
+    'What are your favourite family vacation?',
+    'Did you have best friend and you have to and how did the relationship play out?',
+    'How did you enjoy working',
+    'What are most achievement you are most proud of?'
   ];
 
   @override
@@ -105,24 +70,37 @@ class _MyHomePageState extends State<MyHomePage> {
             SizedBox(
               height: 25,
             ),
-            question[indexx],
+            InkWell(
+              onTap: () {},
+              child: Container(
+                alignment: Alignment.center,
+                height: 150,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Appcolor.cnColor,
+                      width: 2,
+                    ),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10)),
+                child: Text(
+                  que[indexx],
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.ptSerif(
+                      fontWeight: FontWeight.bold,
+                      color: Appcolor.cnColor,
+                      fontSize: 33),
+                ),
+              ),
+            ),
             SizedBox(
               height: 20,
             ),
             Builder(builder: (BuildContext context) {
               return InkWell(
                 onTap: () {
-                  setState(() {
-                    indexx++;
-                    if (question[indexx] == 2) {
-                      Scaffold.of(context).showSnackBar(SnackBar(
-                          content: Text('Waring- please stop tapping')));
-                    }
-                    // if (question[indexx] == 2) {
-                    //   return showToast(
-                    //       title: 'Warning', content: 'Please stop tappping');
-                    // }
-                  });
+                  showDialog(
+                      context: context, builder: (context) => errorDialog);
                 },
                 child: cont1(
                   clr: Appcolor.cnColor,
@@ -133,28 +111,41 @@ class _MyHomePageState extends State<MyHomePage> {
             SizedBox(
               height: 20,
             ),
-            Container(
-              width: 200,
-              padding: EdgeInsets.only(left: 15, right: 15, top: 5, bottom: 5),
-              decoration: BoxDecoration(
-                border: Border.all(color: Appcolor.cnColor, width: 2),
-                borderRadius: BorderRadius.circular(15),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Icon(
-                    Icons.copy,
-                    color: Appcolor.cnColor,
-                  ),
-                  Text(
-                    'try another one',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
+            InkWell(
+              onTap: () {
+                setState(() {
+                  indexx++;
+                  print(indexx);
+                  // if (int.parse(que[indexx]) < que.length) {
+                  //   Scaffold.of(context).showSnackBar(
+                  //       SnackBar(content: Text('Waring- please stop tapping')));
+                  // }
+                });
+              },
+              child: Container(
+                width: 200,
+                padding:
+                    EdgeInsets.only(left: 15, right: 15, top: 5, bottom: 5),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Appcolor.cnColor, width: 2),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Icon(
+                      Icons.copy,
                       color: Appcolor.cnColor,
                     ),
-                  ),
-                ],
+                    Text(
+                      'try another one',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        color: Appcolor.cnColor,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
@@ -173,3 +164,67 @@ void showToast({String? title, String? content}) {
     backgroundColor: Colors.green,
   );
 }
+
+Dialog errorDialog = Dialog(
+  shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(12.0)), //this right here
+  child: Container(
+    padding: EdgeInsets.all(10),
+    height: 400,
+    width: 350,
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        SizedBox(height: 50, child: Image.asset('name')),
+        Text(
+          'Download the best 100 question',
+          style: TextStyle(
+            color: Appcolor.cnColor,
+            fontSize: 27,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          'We would love you to send a ebook for 100 top quetion ',
+          style: TextStyle(
+            color: Appcolor.cnColor,
+            fontSize: 15,
+            fontWeight: FontWeight.normal,
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.grey,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: TextFormField(
+            decoration: InputDecoration(
+                contentPadding: EdgeInsets.only(left: 10),
+                hintText: 'E-mail',
+                hintStyle: TextStyle(
+                    color: Appcolor.cnColor, fontWeight: FontWeight.bold)),
+          ),
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        InkWell(
+          onTap: () {},
+          child: Container(
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Appcolor.cnColor),
+            height: 40,
+            width: 200,
+            child: Text(
+              'Get the E-book',
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+          ),
+        )
+      ],
+    ),
+  ),
+);
